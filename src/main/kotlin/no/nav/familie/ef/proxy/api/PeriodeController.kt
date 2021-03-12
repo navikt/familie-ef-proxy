bbec61eda4aff786799dbe17bacac56ceb893f59
Johan Blomgren <blommish@users.noreply.github.com>
2021-03-12 07:31:26 +0100

    Legger til slik at ef-proxy henter perioder fra integrasjoner i en overgangsperiode då ef-sak ikke har helt tilgang i prod ennå

--- src/main/kotlin/no/nav/familie/ef/proxy/api/PeriodeController.kt
+++ src/main/kotlin/no/nav/familie/ef/proxy/api/PeriodeController.kt
@@ -1,10 +1,10 @@
 package no.nav.familie.ef.proxy.api
 
-import no.nav.familie.ef.proxy.integration.SakClient
+import no.nav.familie.ef.proxy.integration.FamilieIntegrasjonerClient
 import no.nav.familie.ef.proxy.security.StsValidator
+import no.nav.familie.kontrakter.felles.ef.PerioderOvergangsstønadRequest
+import no.nav.familie.kontrakter.felles.ef.PerioderOvergangsstønadResponse
 import no.nav.security.token.support.core.api.Protected
-import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -15,13 +15,16 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(path = ["/api/ekstern/periode/overgangsstonad"],
                 consumes = [MediaType.APPLICATION_JSON_VALUE],
                 produces = [MediaType.APPLICATION_JSON_VALUE])
-class PeriodeController(private val sakClient: SakClient, private val stsValidator: StsValidator) {
+class PeriodeController(private val familieIntegrasjonerClient: FamilieIntegrasjonerClient,
+                        //private val sakClient: SakClient, // TODO skal hente perioder fra ef-sak når er-sak er oppe og kjører
+                        private val stsValidator: StsValidator) {
 
     @PostMapping
     @Protected
-    fun hentPerioder(@RequestBody request: Any): Any {
+    fun hentPerioder(@RequestBody request: PerioderOvergangsstønadRequest): PerioderOvergangsstønadResponse {
         stsValidator.validateSts("srvArena")
-        return sakClient.post(request, "api/ekstern/periode/overgangsstonad/azure")
+        //return sakClient.post(request, "api/ekstern/periode/overgangsstonad/azure")
+        return familieIntegrasjonerClient.hentInfotrygdPerioder(request)
     }
 
 }
