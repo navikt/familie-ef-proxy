5a6b3ce460a9f1ca23175582f8fd19d9f6e9ac07
Johan Blomgren <blommish@users.noreply.github.com>
2021-03-22 14:41:40 +0100

    Legger til endepunkt som heter /perioder då det er tydligere att det ikke kun er perioder for overgangsstønad

--- src/main/kotlin/no/nav/familie/ef/proxy/integration/SakClient.kt
+++ src/main/kotlin/no/nav/familie/ef/proxy/integration/SakClient.kt
@@ -1,6 +1,9 @@
 package no.nav.familie.ef.proxy.integration
 
 import no.nav.familie.http.client.AbstractPingableRestClient
+import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.familie.kontrakter.felles.ef.PerioderOvergangsstønadResponse
+import no.nav.familie.kontrakter.felles.getDataOrThrow
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
@@ -13,9 +16,10 @@ class SakClient(@Qualifier("azure") restOperations: RestOperations,
                 @Value("\${EF_SAK_URL}") private val uri: URI)
     : AbstractPingableRestClient(restOperations, "familie.sak") {
 
-    fun post(data: Any, path: String): Any {
+    fun post(data: Any, path: String): PerioderOvergangsstønadResponse {
         val uri = UriComponentsBuilder.fromUri(uri).pathSegment(path).build().toUri()
-        return postForEntity(uri, data)
+        val postForEntity = postForEntity<Ressurs<PerioderOvergangsstønadResponse>>(uri, data)
+        return postForEntity.getDataOrThrow()
     }
 
     override val pingUri: URI = UriComponentsBuilder.fromUri(uri).pathSegment("api/ping").build().toUri()
---
+++ src/test/kotlin/no/nav/familie/ef/proxy/api/PeriodeControllerTest.kt
@@ -0,0 +1,66 @@
+package no.nav.familie.ef.proxy.api
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.okJson
+import com.github.tomakehurst.wiremock.client.WireMock.post
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import io.mockk.mockk
+import no.nav.familie.ef.proxy.integration.FamilieIntegrasjonerClient
+import no.nav.familie.ef.proxy.integration.SakClient
+import no.nav.familie.ef.proxy.security.StsValidator
+import no.nav.familie.kontrakter.felles.ef.PerioderOvergangsstønadRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.web.client.RestOperations
+import java.net.URI
+
+internal class PeriodeControllerTest {
+
+    @Test
+    internal fun `skal hente perioder`() {
+        val response = periodeController.hentPerioder(PerioderOvergangsstønadRequest(""))
+        assertThat(response.perioder).isEmpty()
+    }
+
+    companion object {
+
+        private val restOperations: RestOperations = RestTemplateBuilder().build()
+        lateinit var wiremockServerItem: WireMockServer
+        lateinit var periodeController: PeriodeController
+
+        @BeforeAll
+        @JvmStatic
+        fun initClass() {
+            wiremockServerItem = WireMockServer(wireMockConfig().dynamicPort())
+            wiremockServerItem.start()
+            val stsValidator = mockk<StsValidator>(relaxed = true)
+            val integrasjonerUri = URI.create("http://localhost:${wiremockServerItem.port()}/integrasjoner")
+            val sakUri = URI.create("http://localhost:${wiremockServerItem.port()}/sak")
+
+            val response = """{"status": "SUKSESS", "data": {"perioder":[]}, "melding": ""}"""
+
+            wiremockServerItem.stubFor(post(urlEqualTo("/integrasjoner/api/infotrygd/vedtak-perioder")).willReturn(okJson(response)))
+            wiremockServerItem.stubFor(post(urlEqualTo("/sak/api/ekstern/perioder")).willReturn(okJson(response)))
+
+            val familieIntegrasjoner = FamilieIntegrasjonerClient(restOperations, integrasjonerUri)
+            val sakClient = SakClient(restOperations, sakUri)
+            periodeController = PeriodeController(familieIntegrasjoner, sakClient, stsValidator)
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun tearDown() {
+            wiremockServerItem.stop()
+        }
+    }
+
+    @AfterEach
+    fun tearDownEachTest() {
+        wiremockServerItem.resetAll()
+    }
+}
