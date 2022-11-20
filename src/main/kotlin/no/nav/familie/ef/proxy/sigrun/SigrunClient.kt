8d9ba2cce349f58d43cea58ca7e4d979d0fda447
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-11-20 19:15:18 +0100

    Integrerer mot Sigrun

---
+++ src/main/kotlin/no/nav/familie/ef/proxy/sigrun/SigrunClient.kt
@@ -0,0 +1,29 @@
+package no.nav.familie.ef.proxy.sigrun
+
+import no.nav.familie.http.client.AbstractRestClient
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestOperations
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+
+@Component
+class SigrunClient(
+    @Value("\${SIGRUN_URL}") private val uri: URI,
+    restOperations: RestOperations
+) : AbstractRestClient(restOperations, "sigrun") {
+
+    fun hentBeregnetSkatt(personIdent: String, inntektsår: Int): Map<String, Any> {
+        val uriComponentsBuilder = UriComponentsBuilder.fromUri(uri).pathSegment("beregnetskatt")
+            .queryParam("inntektsaar", inntektsår)
+
+        return postForEntity(uriComponentsBuilder.build().toUri(), mapOf("fnr" to personIdent))
+    }
+
+    fun hentSummertSkattegrunnlag(personIdent: String, inntektsår: Int): Map<String, Any> {
+        val uriComponentsBuilder = UriComponentsBuilder.fromUri(uri).pathSegment("summertskattegrunnlag")
+            .queryParam("inntektsaar", inntektsår)
+
+        return postForEntity(uriComponentsBuilder.build().toUri(), mapOf("fnr" to personIdent))
+    }
+}
