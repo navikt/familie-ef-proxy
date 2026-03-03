969304112737fb873e5b2e7558a23484b11970c2
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2026-03-03 09:39:51 +0100

    Jackson 3 mapper (#410)
    * ⬆️ Oppgraderer mapper til jackson 3

--- src/main/kotlin/no/nav/familie/ef/proxy/config/RestTemplateSts.kt
+++ src/main/kotlin/no/nav/familie/ef/proxy/config/RestTemplateSts.kt
@@ -1,14 +1,14 @@
 package no.nav.familie.ef.proxy.config
 
-import no.nav.familie.http.config.RestTemplateBuilderBean
-import no.nav.familie.http.interceptor.ConsumerIdClientInterceptor
-import no.nav.familie.http.interceptor.MdcValuesPropagatingClientInterceptor
-import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.kontrakter.felles.jsonMapper
+import no.nav.familie.restklient.config.RestTemplateBuilderBean
+import no.nav.familie.restklient.interceptor.ConsumerIdClientInterceptor
+import no.nav.familie.restklient.interceptor.MdcValuesPropagatingClientInterceptor
 import org.springframework.boot.restclient.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter
 import org.springframework.web.client.RestOperations
 import org.springframework.web.client.RestTemplate
 
@@ -23,7 +23,7 @@ class RestTemplateSts {
     ): RestOperations =
         restTemplateBuilder
             .additionalMessageConverters(
-                listOf(MappingJackson2HttpMessageConverter(objectMapper)) + RestTemplate().messageConverters,
+                listOf(JacksonJsonHttpMessageConverter(jsonMapper)) + RestTemplate().messageConverters,
             ).additionalInterceptors(
                 consumerIdClientInterceptor,
                 MdcValuesPropagatingClientInterceptor(),
